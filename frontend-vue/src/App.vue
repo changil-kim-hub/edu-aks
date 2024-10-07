d60656edeb2154c1159efe88cca9382c2fae7489
changil-kim-hub <ckddlf6904@naver.com>
2024-10-07 16:44:41 +0900

    Update App.vue

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <div>수정완료</div>
+    <div>안녕하세요~ CICD입니다~</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
